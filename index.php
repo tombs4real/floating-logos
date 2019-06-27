ed94380a5e83f4fd51d38c46f7a26c2f6208061f
James Tombs <james.tombs@utah.edu>
2019-06-26 21:08:37 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <title></title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/main.css">
+    <!-- end css -->
+  </head>
+  <body>
+
+    <!-- page -->
+    <div class="page-container">
+
+      <!-- header -->
+      <header></header>
+      <!-- end header -->
+
+      <!-- main content -->
+      <main>
+
+        <!-- article -->
+        <article>
+
+          <!-- logo-bubbles -->
+
+
+<div class="floating-logos-container">
+  <div class="floating-logos"></div>
+</div>
+
+
+          <!-- end logo-bubbles -->
+
+        </article>
+        <!-- end article -->
+
+      </main>
+      <!-- end main content -->
+
+      <!-- footer -->
+      <footer></footer>
+      <!-- end footer -->
+
+    </div>
+    <!-- end page -->
+
+    <!-- scripts -->
+    <script src="https://cdn.rawgit.com/josephg/noisejs/master/perlin.js"></script>
+    <script src="js/dev/custom.js"></script>
+    <!-- end scripts -->
+  </body>
+</html>
